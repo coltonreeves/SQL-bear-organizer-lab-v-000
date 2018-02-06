5253aa50808dc2db57653e590e9653600cf4c054
coltonreeves <coltonallenreeves@gmail.com>
2018-02-06 17:18:15 -0500

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 5, "M", "black", 'docile', 1);
+INSERT INTO bears VALUES (2,'Rowdy', 4, 'F', 'brown', 'docile', 1);
+INSERT INTO bears VALUES (3,'Tabitha', 3, 'M', 'black', 'docile', 1);
+INSERT INTO bears VALUES (4,'Seargent Brown', 2, 'F', 'brown', 'docile', 1);
+INSERT INTO bears VALUES (5,'Melissa', 8, 'M', 'purple', 'docile', 1);
+INSERT INTO bears VALUES (6,'Grinch', 10, 'M', 'grey', 'docile', 1);
+INSERT INTO bears VALUES (7,'Wendy', 40, 'M', 'white', 'docile', 1);
+INSERT INTO bears VALUES (8,null, 80, "F", "green", "docile", 1);
